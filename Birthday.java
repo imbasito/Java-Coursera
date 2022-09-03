5b5b8e4092e17abed35aa53991ee58a80777768c
BASITO <bestboy717@gmail.com>
2022-09-03 22:39:00 +0500

    Lecture 3
    These are Lecture 3 Assignments

---
+++ Birthday.java
@@ -0,0 +1,42 @@
+/* *****************************************************************************
+ *  Name:              Basito
+ *  Coursera User ID:  000000
+ *  Last modified:     03/09/2022
+ **************************************************************************** */
+
+public class Birthday {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        int i = 1;
+        double fraction = 0;
+        double[] peopleCount = new double[n + 2];
+
+        while (fraction < 0.50 && i <= n) {
+            for (int j = 0; j < trials; j++) {
+                int found = 0;
+                boolean[] birthArr = new boolean[n];
+                int enterPeople = 1;
+
+                while (enterPeople < i + 1) {
+                    int birthday = (int) (Math.random() * n);
+                    enterPeople++;
+
+                    if (!birthArr[birthday])
+                        birthArr[birthday] = true;
+                    else
+                        found++;
+                }
+
+                peopleCount[i] += found;
+            }
+            fraction = peopleCount[i] / trials;
+
+            System.out.println(
+                    i + "\t" + (int) (peopleCount[i] - peopleCount[i - 1]) + "\t" + fraction);
+            i++;
+        }
+
+    }
+}
