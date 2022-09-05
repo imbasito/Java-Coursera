944e5601d0bdd190ac9db5f5e016eae287ebc0fa
imbasito <bestboy717@gmail.com>
2022-09-05 10:50:35 +0500

    Nothing changed

---
+++ Practice Files/add_Std.java
@@ -0,0 +1,9 @@
+public class add_Std {
+    public static void main(String[] args) {
+        StdOut.print("Enter 1st value: ");
+        int x = StdIn.readInt();
+        StdOut.print("Enter 2nd value: ");
+        int i = StdIn.readInt();
+        StdOut.print("The addition is: " + x + i);
+    }
+}
