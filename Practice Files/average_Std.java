b16dcb8f6dfda3e55b2ce67a87765a62d970c7e9
imbasito <bestboy717@gmail.com>
2022-09-05 11:14:19 +0500

    ok

--- Practice Files/average_Std.java
+++ Practice Files/average_Std.java
@@ -3,10 +3,14 @@ public class average_Std {
 
         double sum = 0.0;
         int n = 0;
+        StdOut.println("Enter a values: ");
         while (!StdIn.isEmpty()) {
-            double x = StdIn.readDouble();
+            double x = StdIn.readDouble(); // after type the value press enter to type the second value and then press
+                                           // 'ctrl+z' and hit enter to see the result 'ctrl +z' is actually is end
+                                           // after typing the required inputs.
             sum += x;
+            n++;
         }
-        StdOut.print("The average is: " + sum / n);
+        StdOut.println("The average is: " + sum / n);
     }
 }
